adf043675840f587dd08ec696076359ee94cd208
Taim-Amer <taim.amer.000@gmail.com>
2024-11-12 00:58:00 -0800

    Localization Setup v2

--- lib/features/debts/repositories/home/home_repo_impl.dart
+++ lib/features/debts/repositories/home/home_repo_impl.dart
@@ -5,14 +5,14 @@ import 'package:taha_debts/utils/api/dio_helper.dart';
 import 'package:taha_debts/utils/constants/api_constants.dart';
 import 'package:taha_debts/utils/storage/cache_helper.dart';
 
-class HomeRepositoryImpl implements HomeRepository{
+class HomeRepositoryImpl implements HomeRepository {
   static HomeRepositoryImpl get instance => Get.find();
 
   final token = TCacheHelper.getData(key: "token");
 
   @override
-  Future<MyDebtsModel> getMyDebts(String? filter) async{
+  Future<MyDebtsModel> getMyDebts(String? filter) async {
     final dioHelper = TDioHelper();
-    return await dioHelper.get(TApiConstants.getMyDebts, token: token, queryParameters: {"" : filter}).then((response) => MyDebtsModel.fromJson(response));
+    return await dioHelper.get(TApiConstants.getMyDebts, token: token, queryParameters: {"": filter}).then((response) => MyDebtsModel.fromJson(response));
   }
 }
